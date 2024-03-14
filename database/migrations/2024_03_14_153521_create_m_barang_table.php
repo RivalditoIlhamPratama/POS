c784a65360948d68cd6fb647de71206d05fab929
RivalditoIlhamPratama <rivaldodito100@gmail.com>
2024-03-14 22:50:50 +0700

    m_barang

--- database/migrations/2024_03_14_153521_create_m_barang_table.php
+++ database/migrations/2024_03_14_153521_create_m_barang_table.php
@@ -11,8 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('m_barang', function (Blueprint $table) {
-            //
+        Schema::create('m_barang', function (Blueprint $table) {
+            $table->id('barang_id');
+            $table->unsignedBigInteger('kategori_id')->index();
+            $table->string('barang_kode', 10)->unique();
+            $table->string('barang_nama', 100);
+            $table->integer('harga_beli');
+            $table->integer('harga_jual');
+            $table->timestamps();
+
+            $table->foreign('kategori_id')->references('kategori_id')->on('m_kategori');
         });
     }
 
@@ -21,8 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('m_barang', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('m_barang');
     }
 };
